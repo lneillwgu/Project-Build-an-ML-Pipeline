6606cd4555204f30ebc3bbd769057ab2be5d6030
lneillwgu <lneill4@wgu.edu>
2024-04-28 15:48:31 -0500

    Successful runs of Step 4

--- main.py
+++ main.py
@@ -78,11 +78,17 @@ def go(config: DictConfig):
            
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "components", "train_val_test_split"),
+                 "main",
+            parameters={
+                "input":"clean_sample.csv:latest" ,
+                "test_size":config["modeling"]["test_size"],
+                "random_seed":config["modeling"]["random_seed"],
+                "stratify_by":config["modeling"]["stratify_by"]
+            },
+         )    
+            
         if "train_random_forest" in active_steps:
 
             # NOTE: we need to serialize the random forest configuration into JSON
@@ -93,11 +99,21 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                 "main",
+            parameters={
+                "trainval_artifact": "trainval_data.csv:latest",
+                "val_size": config["modeling"]["val_size"],
+                "random_seed": config["modeling"]["random_seed"],
+                "stratify_by": config["modeling"]["stratify_by"],
+                "rf_config": rf_config,
+                "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                "output_artifact": config["modeling"]["output_artifact"] 
+            },
+         )    
 
-            pass
+            
 
         if "test_regression_model" in active_steps:
 
